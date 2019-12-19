b65a5700946fc4c5d984f6271fee5fa6c75058fe
Alex Borsbach <sascha.borsbach@msn.com>
2019-12-19 14:39:23 +0100

    upload after react hooks

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
+import Person from './Person/Person'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-      </div>
-    );
+const app = props => {
+  const [ personsState, setPersonsState ] = useState({
+    persons: [
+      { name: 'Alex', age: 43 },
+      { name: 'Julia', age: 36 },
+      { name: 'Anthony', age: 11 }
+    ],
+    otherState: 'some other value'
+  })
+
+  console.log(personsState)
+
+  const switchNameHandler = () => {
+    // console.log('was clicked')
+    setPersonsState({
+      persons: [
+        { name: 'Alexander', age: 43 },
+        { name: 'Julia', age: 36 },
+        { name: 'Anthony', age: 13 }
+      ]
+    })
   }
+
+  return (
+    <div className="App">
+      <h1>Hi, I'm a React App</h1>
+      <p>This really works!</p>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age} />
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age} >My Hobbies: Video Games</Person>
+    </div>
+  );
 }
 
-export default App;
+
+export default app;
+
+
+
+
